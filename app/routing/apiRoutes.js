09f6a22f6c2318a79df20835f29b1f6b8eb8fca9
shadowlaq <escamillaa@email.arizona.edu>
2018-07-29 22:38:19 -0700

    remove more debugging

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,43 +14,35 @@ module.exports = function(app) {
 
 	// Add new friend entry
 	app.post('/api/friends', function(req, res) {
-		// Capture the user input object
-		var userInput = req.body;
+		// User input (object)
+		var userInput = req.body; //Make a path since I will use this a lot
 
-		var userResponses = userInput.scores;
+		var userResponses = userInput.scores; //second most used path in object
 
 		// Compute best friend match
-		var matchName = '';
-		var matchImage = '';
+		var matchName = ''; //yet to be determined
+		var matchImage = ''; //determined once name is determined
 		var totalDifference = 10000; // Make the initial value big for comparison
 
 		// Examine all existing friends in the list
 		for (var i = 0; i < friends.length; i++) {
-			// console.log('friend = ' + JSON.stringify(friends[i]));
-
 			// Compute differenes for each question
 			var diff = 0;
 			for (var j = 0; j < userResponses.length; j++) {
 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
 			}
-			// console.log('diff = ' + diff);
-
 			// If lowest difference, record the friend match
 			if (diff < totalDifference) {
-				// console.log('Closest match found = ' + diff);
-				// console.log('Friend name = ' + friends[i].name);
-				// console.log('Friend image = ' + friends[i].photo);
-
 				totalDifference = diff;
 				matchName = friends[i].name;
 				matchImage = friends[i].photo;
 			}
 		}
 
-		// Add new user
+		// Add new user ... will reset once server terminated
 		friends.push(userInput);
 
-		// Send appropriate response
+		// Send match response data
 		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 	});
 };
